3ae4cb282c40843904c210c7994057df333b0da1
Mithun Kundu <mithun.kundu@hyland.com>
2018-09-04 15:35:12 +0530

    Display second

--- js/utility.js
+++ js/utility.js
@@ -1,12 +1,14 @@
 function getTimeString(sec) {
     var min=0;
-    if(sec>=60){
-        min=sec/60;
-        sec=sec%60;
+    if(sec>=60) {
+        min = sec / 60;
+        sec = sec % 60;
     }
-    if(sec<=9)
-        sec='0'+sec;
-    var time=Math.floor(min)+":"+ parseInt(sec, 10);
+
+    var dispSec = parseInt(sec, 10);
+    if(dispSec<=9)
+        dispSec = '0'+dispSec;
+    var time=Math.floor(min)+":"+ dispSec;
     return time;
 }
 
